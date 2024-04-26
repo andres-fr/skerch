c22347ada0f3d1846b62634d919373dc43f3acbc
andres-fr <fr_andres@msn.com>
2024-04-26 02:47:25 +0200

    finished main diag utests. moving onto subdiags

--- test/test_subdiagonals.py
+++ test/test_subdiagonals.py
@@ -10,129 +10,203 @@ import torch
 
 from skerch.subdiagonals import subdiagpp
 from skerch.synthmat import SynthMat
-from skerch.utils import gaussian_noise
 
 from . import rng_seeds, torch_devices  # noqa: F401
-
-
-import matplotlib.pyplot as plt
 
 
 # ##############################################################################
 # # FIXTURES
 # ##############################################################################
 @pytest.fixture
-def diag_dist_rtol():
-    """Relative Frobenius error tolerance for the diagonal estimates."""
-    result = 0.05
-    return result
-
-
-@pytest.fixture
-def dim_rank_diags_meas_defl(request):
-    """Test cases for matrix sizes, ranks and subdiagpp measurements
+def dim_rank_decay_sym_diags_meas_defl_rtol(request):
+    """Test cases for main diagonal estimation on different matrices.
 
     Entries are in the form
-    ``(dims, rank, (diag_idxs), num_meas, defl)``, where dims is the number of
-    dimensions of a square test matrix of exp-decaying singular values with
-    given minimum ``rank``. The ``num_meas`` parameter specifies the number of
-    measurements for the hutchinson diagonal estimator, and ``defl`` the rank
-    of the deflation orthogonal projector.
+    ``(dim, rank, decay, sym, [diag_idxs], num_meas, defl, rtol)``, where:
+
+    * ``dim`` is the number of dimensions of a square test matrix of
+      exp-decaying singular values
+    * ``rank`` is the number of unit singular values in the test matrix
+    * ``decay`` is the speed of the exp-decay of singular values after ``rank``.
+      The larger, the faster decay.
+    * ``sym`` is a boolean specifying whether the matrix is symmetric
+    * ``diag_idxs`` is a collection of integers specifying the diagonal indices
+      to be tested: 0 is the main diagonal, 1 the diagonal above, -1 the
+      diagonal below, and so on.
+    * ``num_meas`` specifies the number of measurements for the Hutchinson
+      diagonal estimator
+    * ``defl`` specifies the rank of the orthogonal projector for the
+      Hutchinson++ deflation.
 
     .. note::
 
       The given diagonal indices should not exceed the furthest away
       diagonal, which depends on the shape.
+
+    This is a bit of a complicated fixture, the reason being that the tolerance
+    and number of measurements needed depend heavily on the type of matrix
+    encountered: symmetric random matrices have strong diagonals, which lead
+    to much faster Hutchinson convergence. Low-rank matrices can be effectively
+    characterized via deflation, which also helps. And so on.
     """
+    dims, rank = 1000, 100
+    if request.config.getoption("--quick"):
+        dims, rank = 500, 50
     result = [
-        (1000, 0, [0], 750, 250),
-        # (1000, 200, torch.arange(-9, 9).tolist(), 0, 200),
-        # (10, 5, torch.arange(-9, 9).tolist(), 10),
-        # (100, 20, torch.arange(-99, 100, 99).tolist(), 20),
+        # fast-decay: just Hutch does poorly, but better if symmetric
+        (dims, rank, 0.5, True, [0], round(dims * 0.995), 0, 0.02),
+        (dims, rank, 0.5, False, [0], round(dims * 0.995), 0, 0.2),
+        # slow-decay: just Hutch behaves the same as with fast decay
+        (dims, rank, 0.01, True, [0], round(dims * 0.995), 0, 0.02),
+        (dims, rank, 0.01, False, [0], round(dims * 0.995), 0, 0.2),
+        # fast-decay: just deflating is great (also for asymmetric)
+        (dims, rank, 0.5, True, [0], 0, rank + 10, 1e-4),
+        (dims, rank, 0.5, False, [0], 0, rank + 10, 1e-4),
+        # slow-decay: deflating is less good and affected by asym
+        (dims, rank, 0.01, True, [0], 0, rank * 3, 0.05),
+        (dims, rank, 0.01, False, [0], 0, rank * 4, 0.1),
+        # slow-decay: A lot of Hutch are needed, but deflation tends to help
+        # fo asym
+        (dims, rank, 0.01, True, [0], round(dims * 0.7), rank * 3, 0.02),
+        (dims, rank, 0.01, False, [0], round(dims * 0.7), rank * 4, 0.1),
     ]
-    # if request.config.getoption("--quick"):
-    #     result = result[:4]
     return result
 
 
 @pytest.fixture
-def exp_decays():
-    """Decay values for the synthetic matrix. The larger, the faster decay."""
-    return [0.01]
+def dim_rank_decay_sym_subdiags_meas_defl_rtol(request):
+    """Test cases for subdiagonal estimation.
+
+    More lightweight than the diagonal counterpart.
+    """
+    dims, rank = 1000, 100
+    if request.config.getoption("--quick"):
+        dims, rank = 500, 50
+    result = [
+        (dims, rank, 0.5, True, [-1, 1], round(dims * 0.995), 0, 0.02),
+        # # fast-decay: just Hutch does poorly, but better if symmetric
+        # (dims, rank, 0.5, True, [0], round(dims * 0.995), 0, 0.02),
+        # (dims, rank, 0.5, False, [0], round(dims * 0.995), 0, 0.2),
+        # # slow-decay: just Hutch behaves the same as with fast decay
+        # (dims, rank, 0.01, True, [0], round(dims * 0.995), 0, 0.02),
+        # (dims, rank, 0.01, False, [0], round(dims * 0.995), 0, 0.2),
+        # # fast-decay: just deflating is great (also for asymmetric)
+        # (dims, rank, 0.5, True, [0], 0, rank + 10, 1e-4),
+        # (dims, rank, 0.5, False, [0], 0, rank + 10, 1e-4),
+        # # slow-decay: deflating is less good and affected by asym
+        # (dims, rank, 0.01, True, [0], 0, rank * 3, 0.05),
+        # (dims, rank, 0.01, False, [0], 0, rank * 4, 0.1),
+        # # slow-decay: A lot of Hutch are needed, but deflation tends to help
+        # # fo asym
+        # (dims, rank, 0.01, True, [0], round(dims * 0.7), rank * 3, 0.02),
+        # (dims, rank, 0.01, False, [0], round(dims * 0.7), rank * 4, 0.1),
+    ]
+    return result
 
 
 # ##############################################################################
 # #
 # ##############################################################################
+def test_diags(
+    rng_seeds,  # noqa: F811
+    torch_devices,  # noqa: F811
+    dim_rank_decay_sym_diags_meas_defl_rtol,
+):
+    """Test diagonal estimation on exponential matrices."""
+    for seed in rng_seeds:
+        for dtype in (torch.float64, torch.float32):
+            for device in torch_devices:
+                for (
+                    dim,
+                    rank,
+                    decay,
+                    sym,
+                    diags,
+                    meas,
+                    defl,
+                    rtol,
+                ) in dim_rank_decay_sym_diags_meas_defl_rtol:
+                    mat = SynthMat.exp_decay(
+                        shape=(dim, dim),
+                        rank=rank,
+                        decay=decay,
+                        symmetric=sym,
+                        seed=seed,
+                        dtype=dtype,
+                        device=device,
+                        psd=False,
+                    )
+                    for diag_idx in diags:
+                        # retrieve the true diag
+                        diag = torch.diag(mat, diagonal=diag_idx)
+                        # matrix-free estimation of the diag
+                        diag_est, _, norms = subdiagpp(
+                            meas,
+                            mat,
+                            dtype,
+                            device,
+                            seed + 1,
+                            defl,
+                            diag_idx,
+                        )
+                        # then assert
+                        dist = torch.dist(diag, diag_est)
+                        rel_err = (dist / torch.norm(diag)).item()
+                        assert rel_err <= rtol, "Incorrect diagonal recovery!"
+
+
 def test_subdiags(
     rng_seeds,  # noqa: F811
     torch_devices,  # noqa: F811
-    diag_dist_rtol,
-    dim_rank_diags_meas_defl,
-    exp_decays,
+    dim_rank_decay_sym_subdiags_meas_defl_rtol,
 ):
-    """Test SSVD on asymmetric exponential matrices.
+    """Test subdiagonal estimation on exponential matrices
 
     Sample random asymmetric matrices, and retrieve arbitrary (sub-)diagonals
     from them. Test that:
     * Sketched diagonals are close to actual diagonals
     * Sketches of linear combinations of diagonals are close to actual ones
     * Lowtri evaluations?
+
     """
     for seed in rng_seeds:
-        for dim, rank, diags, meas, defl in dim_rank_diags_meas_defl:
-            for dtype in (torch.float64, torch.float32):
-                for device in torch_devices:
-                    for decay in exp_decays:
-                        for sym in (True, False):
-                            print(
-                                "\n\n\n\n>>>>",
-                                dim,
-                                rank,
-                                diags,
-                                dtype,
-                                device,
-                                decay,
-                                "SYM" if sym else "ASYM",
-                            )
-                            mat = SynthMat.exp_decay(
-                                shape=(dim, dim),
-                                rank=rank,
-                                decay=decay,
-                                symmetric=False,
-                                seed=seed,
-                                dtype=dtype,
-                                device=device,
-                                psd=False,
-                            )
-
-                            for diag_idx in diags:
-                                diag_idx = 0
-                                # retrieve the true diag
-                                diag = torch.diag(mat, diagonal=diag_idx)
-                                # matrix-free estimation of the diag
-                                diag_est, _, norms = subdiagpp(
-                                    meas,
-                                    mat,
-                                    dtype,
-                                    device,
-                                    seed + 1,
-                                    defl,
-                                    diag_idx,
-                                )
-                                # then assert
-                                dist = torch.dist(diag, diag_est)
-                                rel_err = (dist / torch.norm(diag)).item()
-                                print("\n", rel_err)
-                                breakpoint()
-                                try:
-                                    assert (
-                                        rel_err <= diag_dist_rtol
-                                    ), "Incorrect diagonal recovery!"
-                                except:
-                                    # plt.plot(diag, color="grey"); plt.plot(diag_est); plt.show()
-                                    # plt.plot(diag, color="grey"); plt.plot(diag - diag_est); plt.show()
-
-                                    # plt.hist(diag, bins=100, color="grey"); plt.hist(diag - diag_est, alpha=0.5, bins=100); plt.show()
-
-                                    breakpoint()
+        for dtype in (torch.float64, torch.float32):
+            for device in torch_devices:
+                for (
+                    dim,
+                    rank,
+                    decay,
+                    sym,
+                    diags,
+                    meas,
+                    defl,
+                    rtol,
+                ) in dim_rank_decay_sym_subdiags_meas_defl_rtol:
+                    mat = SynthMat.exp_decay(
+                        shape=(dim, dim),
+                        rank=rank,
+                        decay=decay,
+                        symmetric=sym,
+                        seed=seed,
+                        dtype=dtype,
+                        device=device,
+                        psd=False,
+                    )
+                    for diag_idx in diags:
+                        # retrieve the true diag
+                        diag = torch.diag(mat, diagonal=diag_idx)
+                        # matrix-free estimation of the diag
+                        diag_est, _, norms = subdiagpp(
+                            meas,
+                            mat,
+                            dtype,
+                            device,
+                            seed + 1,
+                            defl,
+                            diag_idx,
+                        )
+                        breakpoint()
+                        # then assert
+                        dist = torch.dist(diag, diag_est)
+                        rel_err = (dist / torch.norm(diag)).item()
+                        assert rel_err <= rtol, "Incorrect diagonal recovery!"
